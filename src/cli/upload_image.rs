8fff3a2b4cf811d809df2fd62f3156631eb9318e
MedzikUser <nivua1fn@duck.com>
2022-01-22 23:19:59 +0100

    this should solve the  in rust nightly

--- src/cli/upload_image.rs
+++ src/cli/upload_image.rs
@@ -10,14 +10,12 @@ use chrono::Utc;
 use std::time::{Duration, UNIX_EPOCH};
 
 pub async fn upload_image(client: ImgurHandle, path: &str) {
-    let image: String;
+    let mut image: String = path.to_string();
 
     if Path::new(path).exists() {
         let bytes = fs::read(path).map_err(|err| err.to_string()).unwrap();
 
         image = base64::encode(bytes);
-    } else {
-        image = path.to_string()
     }
 
     match imgurs::api::upload_image::upload_image(client, &image).await {

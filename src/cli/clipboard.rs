a4a17a4fc5c700602fc22ea31e0eb16153677cb2
Medzik <nivua1fn@duck.com>
2022-02-26 22:51:12 +0100

    fix clipboard

--- src/cli/clipboard.rs
+++ src/cli/clipboard.rs
@@ -2,20 +2,18 @@
     unix,
     not(any(target_os = "macos", target_os = "android", target_os = "emscripten"))
 ))]
-use clipboard_ext::prelude::*;
-#[cfg(all(
-    unix,
-    not(any(target_os = "macos", target_os = "android", target_os = "emscripten"))
-))]
-use clipboard_ext::x11_fork::ClipboardContext;
-
-#[cfg(all(
-    unix,
-    not(any(target_os = "macos", target_os = "android", target_os = "emscripten"))
-))]
 pub fn set_clipboard(content: String) {
-    let mut ctx = ClipboardContext::new().expect("init clipboard");
-    ctx.set_contents(content).expect("set clipboard");
+    use std::{process::{Command, Stdio}, io::Write};
+
+    let mut child = Command::new("xsel")
+        .arg("--input")
+        .arg("--clipboard")
+        .stdin(Stdio::piped())
+        .spawn()
+        .expect("execute command xsel");
+
+    child.stdin.as_mut().unwrap().write_all(content.as_bytes()).expect("execute command");
+    child.wait_with_output().unwrap();
 }
 
 #[cfg(not(all(
